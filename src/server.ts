47fe654fb068440e552e865b8fff2eb4c57235b7
Ian <example@example.com>
2022-03-05 00:30:21 +0100

    Server Track die VoiceChannelZeit

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,35 @@
 require("dotenv").config()
 
-import {Client} from "discord.js"
+import {Client, Intents} from "discord.js"
 
-const client = new Client({intents: []})
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] });
 
-client.once("ready", () => {
-    console.log("Hallo Ian")
-})
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', message => {
+    console.log(message.author.username + ": " + message.content);
+});
+
+const rankings = new Map()
+
+client.on('voiceStateUpdate', (oldState, newState) => {
+    const userId = oldState.member.user.id;
+
+    if (!rankings.has(userId)) {
+        rankings.set(userId, 0)
+    }
+
+    let interval: NodeJS.Timeout = null;
+
+    if (oldState.channelId === null && newState.channelId !== null) {
+        interval = setInterval(function() {
+            rankings.set(userId, rankings.get(userId) + 1)
+        }, 1000)
+    } else if (newState.channelId === null && oldState.channelId !== null) {
+        clearInterval(interval)
+    }
+});
 
 client.login(process.env.DISCORD_BOT_TOKEN)
