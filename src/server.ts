3bafa29d4bdca0e9916ba17e055f47034fa53236
Ian <example@example.com>
2022-03-04 22:26:32 +0100

    Wir haben Discord.js installiert

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("Hallo");
+require("dotenv").config()
+
+import {Client} from "discord.js"
+
+const client = new Client({intents: []})
+
+client.once("ready", () => {
+    console.log("Hallo Ian")
+})
+
+client.login(process.env.DISCORD_BOT_TOKEN)
