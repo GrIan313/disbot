099d9e8eca92a02f3dd64f3c85925873e30c2561
Ian <example@example.com>
2022-03-06 17:08:37 +0100

    Sessiontimer(Timer restart), time Commands + level

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 require("dotenv").config()
 
-import { Client, Intents, Interaction, Message } from "discord.js"
+import { Client, Intents } from "discord.js"
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] });
 
@@ -9,16 +9,24 @@ client.once('ready', () => {
 });
 
 client.on('messageCreate', message => {
-    console.log(message.author.username + ": " + message.content);
+    console.log(message.author.username + " : " + message.content);
 });
 
 // const joinStates = new Map()
-const rankings = new Map()
+const allTime = new Map()
+const sesTime = new Map()
 
 client.on('messageCreate', function (message) {
     const userId = message.member.user.id;
     if (message.content === '/time') {
-        message.channel.send(`${message.member.user.username} ist seit ${rankings.get(userId)} Sekunden auf dem Server.`);
+        message.channel.send(`${message.member.user.username} nervt jetzt schon ${sesTime.get(userId)} Sekunden am Stück.`);
+    }
+});
+
+client.on('messageCreate', function (message) {
+    const userId = message.member.user.id;
+    if (message.content === '/time') {
+        message.channel.send(`${message.member.user.username} labert jetzt schon insgesamt seit ${allTime.get(userId)} Stunden.`);
     }
 });
 
@@ -30,38 +38,98 @@ client.on('messageCreate', function (message) {
         }, 5000)
     })*/
 
+    //Overall Time
 
     client.on('voiceStateUpdate', (oldState, newState) => {
         const userId = oldState.member.user.id;
         const username = oldState.member.user.username;
     
-        if (!rankings.has(userId)) {
-            rankings.set(userId, 0)
+        if (!allTime.has(userId)) {
+            allTime.set(userId, 0)
         }
     
         //let interval: NodeJS.Timeout = null;
     
         if (oldState.channelId === null && newState.channelId !== null) {
             const iid = setInterval(function () {
-                rankings.set(userId, rankings.get(userId) + 1)
-                    console.log(rankings.get(userId))
+                allTime.set(userId, allTime.get(userId) + 1)
+                    console.log(allTime.get(userId))
             if (newState.channelId === null && oldState.channelId === null) {
                 console.log(`${username} hat den Server verlassen.`)
                 clearInterval(iid)
-            }
-    
-                if (rankings.get(userId) === 5) {
-                    console.log(`${username} ist seit 5 Sekunden auf dem Sever.`)
+            }   
+                if (allTime.get(userId) === 10){
+                    client.channels.cache.get('948614963092668449').send(`${username} hat Level 0 erreicht. Herzlichen Glückwunsch!`)
                 }
 
-                if (rankings.get(userId) === 30){
-                    client.channels.cache.get('948614963092668449').send(`${username} ist seit 30 Sekunden auf dem Server.`)
+                if (allTime.get(userId) === 36000){
+                    client.channels.cache.get('948614963092668449').send(`${username} hat Level 1 erreicht. Herzlichen Glückwunsch!`)
                 }
 
-            }, 1000)   
+                if (allTime.get(userId) === 54000){
+                    client.channels.cache.get('948614963092668449').send(`${username} hat Level 2 erreicht. Herzlichen Glückwunsch!`)
+                }
+
+                if (allTime.get(userId) === 81000){
+                    client.channels.cache.get('948614963092668449').send(`${username} hat Level 3 erreicht. Herzlichen Glückwunsch!`)
+                }
+
+                if (allTime.get(userId) === 121500){
+                    client.channels.cache.get('948614963092668449').send(`${username} hat Level 4 erreicht. Herzlichen Glückwunsch!`)
+                }
+
+                if (allTime.get(userId) === 182250){
+                    client.channels.cache.get('948614963092668449').send(`${username} hat Level 5 erreicht. Herzlichen Glückwunsch!`)
+                }
+
+                if (allTime.get(userId) === 273375){
+                    client.channels.cache.get('948614963092668449').send(`${username} hat Level 6 erreicht. Herzlichen Glückwunsch!`)
+                }
+
+                if (allTime.get(userId) === 410062){
+                    client.channels.cache.get('948614963092668449').send(`${username} hat Level 7 erreicht. Herzlichen Glückwunsch!`)
+                }
+
+                if (allTime.get(userId) === 615093){
+                    client.channels.cache.get('948614963092668449').send(`${username} hat Level 8 erreicht. Herzlichen Glückwunsch!`)
+                }
+
+                if (allTime.get(userId) === 922639){
+                    client.channels.cache.get('948614963092668449').send(`${username} hat Level 9 erreicht. Herzlichen Glückwunsch!`)
+                }
+
+                if (allTime.get(userId) === 1383958){
+                    client.channels.cache.get('948614963092668449').send(`${username} hat Level 10 erreicht. Herzlichen Glückwunsch!`)
+                }
+
+            }, 1000)      
+        }     
+    });
+
+    //Session Time
+
+    client.on('voiceStateUpdate', (oldState, newState) => {
+        const userId = oldState.member.user.id;
+        const username = oldState.member.user.username;
     
-        }  
+        if (!sesTime.has(userId)) {
+            sesTime.set(userId, 0)
+        }
+
+        if (sesTime.has(userId)) {
+            sesTime.set(userId, 0)
+        }
     
+        if (oldState.channelId === null && newState.channelId !== null) {
+            var iid = setInterval(function () {
+                sesTime.set(userId, sesTime.get(userId) + 1)
+                    console.log(sesTime.get(userId))
+            if (newState.channelId === null && oldState.channelId === null) {
+                console.log(`${username} hat den Server verlassen.`)
+                clearInterval(iid)
+            }   
+        }, 1000)      
+        }     
     });
 
 client.login(process.env.DISCORD_BOT_TOKEN)
